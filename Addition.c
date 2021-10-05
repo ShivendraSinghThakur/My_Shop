642eaa2892ca6b15b22325f2a6b9d51a1bb3dec5
ShivendraSinghThakur <91961373+ShivendraSinghThakur@users.noreply.github.com>
2021-10-05 19:08:17 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int a,b,sum;
+printf("Enter value of a: ");
+scanf("%d",&a);
+printf("Enter value of b: ");
+scanf("%d",&b);
+sum = a + b;
+printf("Sum of a and b is %d",a+b);
+}
